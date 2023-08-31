2d75092f0e7898aca2cfcf49e84898bf3afd3f61
Lukas Wunner <lukas@wunner.de>
2023-08-31 14:29:09 +0200

    spdm: Add support for large SPDM message transfer mechanism
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- lib/spdm/req-authenticate.c
+++ lib/spdm/req-authenticate.c
@@ -117,7 +117,7 @@ static int spdm_get_capabilities(struct spdm_state *spdm_state)
 		req_sz = sizeof(*req);
 		rsp_sz = sizeof(*rsp);
 		req->data_transfer_size = cpu_to_le32(spdm_state->transport_sz);
-		req->max_spdm_msg_size = cpu_to_le32(spdm_state->transport_sz);
+		req->max_spdm_msg_size = cpu_to_le32(UINT_MAX);
 	}
 
 	rsp = spdm_state->transcript_end += req_sz;
