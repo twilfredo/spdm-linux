14831b2a3b1b971e09ef4f870b296fd01a6ebb84
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2024-08-30 10:27:48 +1000

    spdm: req-sysfs: add NVMe device to SPDM state
    If SPDM state has been established using the NVMe Admin Security
    Send/Receive commands as SPDM transport, allow the req-sysfs to fetch
    the respective SPDM state from the device.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- drivers/nvme/host/nvme-spdm.c
+++ drivers/nvme/host/nvme-spdm.c
@@ -126,6 +126,21 @@ void nvme_spdm_init(struct device *dev)
 	queue_work(nvme_spdm_wq,  &ctrl->spdm_work);
 }
 
+bool dev_is_nvme(struct device *dev)
+{
+	if (dev && strncmp(dev_name(dev), "nvme", 4) == 0)
+		return true;
+
+	return false;
+}
+
+struct spdm_state *nvme_dev_to_spdm_state(struct device *dev)
+{
+	struct nvme_ctrl *ctrl = dev_get_drvdata(dev);
+
+	return ctrl ? ctrl->spdm_state : NULL;
+}
+
 #ifdef CONFIG_SYSFS
 const struct attribute_group *nvme_spdm_attr_groups[] = {
 	&spdm_attr_group,
