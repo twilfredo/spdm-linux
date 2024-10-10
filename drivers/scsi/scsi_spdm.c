1c190b1e84ca6aabd8cd81713bc208bc93fb541f
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2024-10-10 18:55:11 +1000

    spdm: req-sysfs: add SCSI device to SPDM state
    If SPDM state has been established using the SCSI Security
    In/Out commands as SPDM transport, allow the req-sysfs to fetch
    the respective SPDM state from the device.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- drivers/scsi/scsi_spdm.c
+++ drivers/scsi/scsi_spdm.c
@@ -125,6 +125,21 @@ void scsi_spdm_init(struct device *dev)
 	queue_work(scsi_spdm_wq,  &sdkp->spdm_work);
 }
 
+bool dev_is_scsi(struct device *dev)
+{
+	if (dev && dev->parent->bus && !strcmp(dev->parent->bus->name, "scsi"))
+		return true;
+
+	return false;
+}
+
+struct spdm_state *scsi_dev_to_spdm_state(struct device *dev)
+{
+	struct scsi_disk *sdkp = dev_get_drvdata(dev);
+
+	return sdkp ? sdkp->spdm_state : NULL;
+}
+
 #ifdef CONFIG_SYSFS
 const struct attribute_group *scsi_spdm_attr_groups[] = {
 	&spdm_attr_group,
