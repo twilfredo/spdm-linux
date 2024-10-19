ce5daf15d93a94d91269f2e3a2b93ac796bad013
Lukas Wunner <lukas@wunner.de>
2024-10-19 10:52:36 +0200

    KEYS: asymmetric: Use scope-based allocation in software_key_query()
    Simplify software_key_query() error paths by taking advantage of
    __free(kfree_sensitive) for the "key" allocation.
    
    No functional change intended.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- crypto/asymmetric_keys/public_key.c
+++ crypto/asymmetric_keys/public_key.c
@@ -165,9 +165,9 @@ static int software_key_query(const struct kernel_pkey_params *params,
 {
 	struct crypto_akcipher *tfm;
 	struct public_key *pkey = params->key->payload.data[asym_crypto];
+	u8 *ptr, *key __free(kfree_sensitive) = NULL;
 	char alg_name[CRYPTO_MAX_ALG_NAME];
 	struct crypto_sig *sig;
-	u8 *key, *ptr;
 	int ret, len;
 	bool issig;
 
@@ -190,10 +190,8 @@ static int software_key_query(const struct kernel_pkey_params *params,
 
 	if (issig) {
 		sig = crypto_alloc_sig(alg_name, 0, 0);
-		if (IS_ERR(sig)) {
-			ret = PTR_ERR(sig);
-			goto error_free_key;
-		}
+		if (IS_ERR(sig))
+			return PTR_ERR(sig);
 
 		if (pkey->key_is_private)
 			ret = crypto_sig_set_privkey(sig, key, pkey->keylen);
@@ -217,10 +215,8 @@ static int software_key_query(const struct kernel_pkey_params *params,
 		}
 	} else {
 		tfm = crypto_alloc_akcipher(alg_name, 0, 0);
-		if (IS_ERR(tfm)) {
-			ret = PTR_ERR(tfm);
-			goto error_free_key;
-		}
+		if (IS_ERR(tfm))
+			return PTR_ERR(tfm);
 
 		if (pkey->key_is_private)
 			ret = crypto_akcipher_set_priv_key(tfm, key, pkey->keylen);
@@ -249,9 +245,6 @@ error_free_tfm:
 		crypto_free_sig(sig);
 	else
 		crypto_free_akcipher(tfm);
-error_free_key:
-	kfree_sensitive(key);
-	pr_devel("<==%s() = %d\n", __func__, ret);
 	return ret;
 }
 
