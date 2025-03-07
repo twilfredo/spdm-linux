3f00b475b74aa12b04b2ddb57f52abb64e679c0f
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2025-03-07 12:17:46 +1000

    nvmet: fabrics: add CQ init and destroy
    With `nvmet_cq`, now supporting reference counting, this patch
    amends the target fabrics call chain to initialize and destroy a
    completion queue. Since fabrics do not share completion queues,
    it would be erroneous for `nvmet_cq_destroy()` to fail. Hence,
    `WARN_ON()` is used to watch for the return value of `nvmet_cq_destroy()`.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- drivers/nvme/target/rdma.c
+++ drivers/nvme/target/rdma.c
@@ -1353,6 +1353,7 @@ static void nvmet_rdma_free_queue(struct nvmet_rdma_queue *queue)
 	pr_debug("freeing queue %d\n", queue->idx);
 
 	nvmet_sq_destroy(&queue->nvme_sq);
+	WARN_ON(nvmet_cq_destroy(&queue->nvme_cq) != NVME_SC_SUCCESS);
 
 	nvmet_rdma_destroy_queue_ib(queue);
 	if (!queue->nsrq) {
@@ -1436,10 +1437,11 @@ nvmet_rdma_alloc_queue(struct nvmet_rdma_device *ndev,
 		goto out_reject;
 	}
 
+	nvmet_cq_init(&queue->nvme_cq);
 	ret = nvmet_sq_init(&queue->nvme_sq);
 	if (ret) {
 		ret = NVME_RDMA_CM_NO_RSC;
-		goto out_free_queue;
+		goto out_destroy_cq_free_queue;
 	}
 
 	ret = nvmet_rdma_parse_cm_connect_req(&event->param.conn, queue);
@@ -1516,7 +1518,8 @@ out_ida_remove:
 	ida_free(&nvmet_rdma_queue_ida, queue->idx);
 out_destroy_sq:
 	nvmet_sq_destroy(&queue->nvme_sq);
-out_free_queue:
+out_destroy_cq_free_queue:
+	WARN_ON(nvmet_cq_destroy(&queue->nvme_cq) != NVME_SC_SUCCESS);
 	kfree(queue);
 out_reject:
 	nvmet_rdma_cm_reject(cm_id, ret);
