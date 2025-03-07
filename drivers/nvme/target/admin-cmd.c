00eeb73ed6b6ca398159fe47a73fbe6c26e3b614
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2025-03-07 12:03:27 +1000

    nvmet: cq: prepare for completion queue sharing
    This is a preparatory patch that sets up allowing for completion queue
    sharing by submission queues.
    
    For PCI, the NVMe specification allows submission queues to share
    completion queues, however, this is not supported in the current
    implementation. To support queue sharing, reference counting
    completion queues is required, this patch adds a `refcount_t` to the
    `nvmet_cq` struct coupled with respective `nvmet_cq_init()` and
    `nvmet_cq_destroy()` functions.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- drivers/nvme/target/admin-cmd.c
+++ drivers/nvme/target/admin-cmd.c
@@ -88,6 +88,7 @@ complete:
 static void nvmet_execute_delete_cq(struct nvmet_req *req)
 {
 	struct nvmet_ctrl *ctrl = req->sq->ctrl;
+	struct nvmet_cq *cq;
 	u16 cqid = le16_to_cpu(req->cmd->delete_queue.qid);
 	u16 status;
 
@@ -96,12 +97,17 @@ static void nvmet_execute_delete_cq(struct nvmet_req *req)
 		goto complete;
 	}
 
-	if (!cqid) {
+	cq = ctrl->cqs[cqid];
+	if (!cqid || !cq) {
 		status = NVME_SC_QID_INVALID | NVME_STATUS_DNR;
 		goto complete;
 	}
 
-	status = nvmet_check_cqid(ctrl, cqid);
+	status = nvmet_check_cqid(ctrl, cqid, false);
+	if (status != NVME_SC_SUCCESS)
+		goto complete;
+
+	status = nvmet_cq_destroy(cq);
 	if (status != NVME_SC_SUCCESS)
 		goto complete;
 
@@ -132,7 +138,7 @@ static void nvmet_execute_create_cq(struct nvmet_req *req)
 		goto complete;
 	}
 
-	status = nvmet_check_cqid(ctrl, cqid);
+	status = nvmet_check_cqid(ctrl, cqid, true);
 	if (status != NVME_SC_SUCCESS)
 		goto complete;
 
