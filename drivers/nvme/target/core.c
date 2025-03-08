7b0da4ef472b6381f816aa7f79a3221f990cbbca
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2025-03-08 13:42:19 +1000

    nvmet: support completion queue sharing
    The NVMe specification for PCI allows for completion queues to be shared
    by submission queues.
    
    This patch allows a submission queue to keep track of the completion queue
    it's using with reference counting. As such, it can be ensured that a
    completion queues is not deleted while a submission queue is actively
    using it.
    
    Completion queue sharing is now enabled for the `nvmet pci-epf` driver.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- drivers/nvme/target/core.c
+++ drivers/nvme/target/core.c
@@ -921,7 +921,7 @@ u16 nvmet_check_sqid(struct nvmet_ctrl *ctrl, u16 sqid,
 }
 
 u16 nvmet_sq_create(struct nvmet_ctrl *ctrl, struct nvmet_sq *sq,
-		    u16 sqid, u16 size)
+		    struct nvmet_cq *cq, u16 sqid, u16 size)
 {
 	u16 status;
 	int ret;
@@ -933,7 +933,7 @@ u16 nvmet_sq_create(struct nvmet_ctrl *ctrl, struct nvmet_sq *sq,
 	if (status != NVME_SC_SUCCESS)
 		return status;
 
-	ret = nvmet_sq_init(sq);
+	ret = nvmet_sq_init(sq, cq);
 	if (ret) {
 		status = NVME_SC_INTERNAL | NVME_STATUS_DNR;
 		goto ctrl_put;
@@ -965,6 +965,7 @@ void nvmet_sq_destroy(struct nvmet_sq *sq)
 	wait_for_completion(&sq->free_done);
 	percpu_ref_exit(&sq->ref);
 	nvmet_auth_sq_free(sq);
+	nvmet_cq_put(sq->cq);
 
 	/*
 	 * we must reference the ctrl again after waiting for inflight IO
@@ -997,18 +998,24 @@ static void nvmet_sq_free(struct percpu_ref *ref)
 	complete(&sq->free_done);
 }
 
-int nvmet_sq_init(struct nvmet_sq *sq)
+int nvmet_sq_init(struct nvmet_sq *sq, struct nvmet_cq *cq)
 {
 	int ret;
+
+	ret = nvmet_cq_get(cq);
+	if (ret)
+		return ret;
 
 	ret = percpu_ref_init(&sq->ref, nvmet_sq_free, 0, GFP_KERNEL);
 	if (ret) {
 		pr_err("percpu_ref init failed!\n");
+		nvmet_cq_put(cq);
 		return ret;
 	}
 	init_completion(&sq->free_done);
 	init_completion(&sq->confirm_done);
 	nvmet_auth_sq_init(sq);
+	sq->cq = cq;
 
 	return 0;
 }
