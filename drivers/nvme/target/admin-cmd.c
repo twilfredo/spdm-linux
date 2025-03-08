7d51435c85d34525155f48ededb9fde8f83be28a
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2025-03-08 13:42:19 +1000

    nvmet: support completion queue sharing
    The NVMe PCI transport specification allows for completion queues to be
    shared by submission queues.
    
    This patch allows a submission queue to keep track of the completion queue
    it is using with reference counting. As such, it can be ensured that a
    completion queue is not deleted while a submission queue is actively
    using it.
    
    Completion queue sharing is now enabled for the nvmet pci-epf driver.
    For fabrics drivers, completion queue sharing is not enabled as it is
    not possible as per the fabrics specification. However, this patch
    ammends the fabrics drivers to corrently integrate the new API that
    supports completion queue sharing.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- drivers/nvme/target/admin-cmd.c
+++ drivers/nvme/target/admin-cmd.c
@@ -63,23 +63,25 @@ static void nvmet_execute_create_sq(struct nvmet_req *req)
 	if (status != NVME_SC_SUCCESS)
 		goto complete;
 
-	/*
-	 * Note: The NVMe specification allows multiple SQs to use the same CQ.
-	 * However, the target code does not really support that. So for now,
-	 * prevent this and fail the command if sqid and cqid are different.
-	 */
-	if (!cqid || cqid != sqid) {
+	if (!cqid) {
 		pr_err("SQ %u: Unsupported CQID %u\n", sqid, cqid);
 		status = NVME_SC_CQ_INVALID | NVME_STATUS_DNR;
 		goto complete;
 	}
+
+	status = nvmet_check_cqid(ctrl, cqid, false);
+	if (status != NVME_SC_SUCCESS) {
+		pr_err("SQ %u: Unsupported CQID %u\n", sqid, cqid);
+		goto complete;
+	}
+
 
 	if (!qsize || qsize > NVME_CAP_MQES(ctrl->cap)) {
 		status = NVME_SC_QUEUE_SIZE | NVME_STATUS_DNR;
 		goto complete;
 	}
 
-	status = ctrl->ops->create_sq(ctrl, sqid, sq_flags, qsize, prp1);
+	status = ctrl->ops->create_sq(ctrl, sqid, cqid, sq_flags, qsize, prp1);
 
 complete:
 	nvmet_req_complete(req, status);
